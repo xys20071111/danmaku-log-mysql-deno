c6ae02c19dcdc9f54e8534a3634772d77ab2dc0b
xys20071111 <xys20071111@hotmail.com>
2022-11-03 20:15:36 +0800

    init

---
+++ src/mod.ts
@@ -0,0 +1,43 @@
+import { EventEmitter } from './deps.ts'
+import { config } from './config.ts'
+import { db } from './db.ts'
+
+db.query('CREATE TABLE IF NOT EXISTS`log` ( `id` INT NOT NULL AUTO_INCREMENT , `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `roomId` INT NOT NULL , `uid` BIGINT UNSIGNED NOT NULL , `nickname` VARCHAR(255) NOT NULL , `text` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+let ws = new WebSocket(`ws://127.0.0.1:${config.apiPort}`)
+const APIMsgHandler = new EventEmitter()
+
+interface IMessage {
+    cmd: string
+    data: any
+}
+
+function registerCallback() {
+    ws.addEventListener('open', () => {
+        ws.send(JSON.stringify({ cmd: "AUTH", data: config.token }))
+    })
+    ws.addEventListener('close', () => {
+        ws = new WebSocket(`ws://127.0.0.1:${config.apiPort}`)
+        registerCallback()
+    })
+    ws.addEventListener('message', (event) => {
+        const msg: IMessage = JSON.parse(event.data)
+        APIMsgHandler.emit(msg.cmd, msg.data)
+    })
+}
+
+registerCallback()
+
+APIMsgHandler.on('AUTH', (result: string) => {
+    if (result === 'AUTHED') {
+        ws.send(JSON.stringify({ cmd: "ROOMID", data: config.token }));
+    } else {
+        console.log('[弹幕日志插件] 认证失败');
+    }
+})
+
+APIMsgHandler.on('ROOMID', (roomId: number) => {
+    console.log(`[弹幕日志插件] 工作在${roomId}`);
+    APIMsgHandler.on('DANMU_MSG', (data: Array<any>) => {
+        db.query('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [roomId, data[2][0], data[2][1], data[1]]);
+    });
+})
