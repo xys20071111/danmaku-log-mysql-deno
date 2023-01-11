0b37ff8d51ca1c2a9269f343054ac56093ff6d7b
xys20071111 <xys20071111@hotmail.com>
2023-01-11 14:18:27 +0800

    把catch改成promise的catch

--- src/mod.ts
+++ src/mod.ts
@@ -2,7 +2,7 @@ import { EventEmitter } from './deps.ts'
 import { config } from './config.ts'
 import { db } from './db.ts'
 
-db.query('CREATE TABLE IF NOT EXISTS`log` ( `id` INT NOT NULL AUTO_INCREMENT , `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `roomId` INT NOT NULL , `uid` BIGINT UNSIGNED NOT NULL , `nickname` VARCHAR(255) NOT NULL , `text` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+db.query('CREATE TABLE IF NOT EXISTS`log` ( `id` INT NOT NULL AUTO_INCREMENT , `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `roomId` INT NOT NULL , `uid` BIGINT UNSIGNED NOT NULL , `nickname` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, `text` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;');
 let ws = new WebSocket(`ws://127.0.0.1:${config.apiPort}`)
 const APIMsgHandler = new EventEmitter()
 
@@ -38,13 +38,12 @@ APIMsgHandler.on('AUTH', (result: string) => {
 APIMsgHandler.on('ROOMID', (roomId: number) => {
     console.log(`[弹幕日志插件] 工作在${roomId}`);
     APIMsgHandler.on('DANMU_MSG', (data: Array<any>) => {
-        try{
-            db.query('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [roomId, data[2][0], data[2][1], data[1]]);
-        } catch (e) {
-            console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`)
-            console.log((e as Error).message)
-        }
-        
+        db.execute('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [roomId, data[2][0], data[2][1], data[1]])
+            .catch((reason) => {
+                console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`)
+                console.log((reason as Error).message)
+            })
+
     });
 })
 
