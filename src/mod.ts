8ebb82c714dc11a6463eae4e95d49996722dfb0b
xys20071111 <xys20071111@hotmail.com>
2023-02-18 10:38:27 +0800

    添加多房间支持

--- src/mod.ts
+++ src/mod.ts
@@ -2,12 +2,13 @@ import { EventEmitter } from './deps.ts'
 import { config } from './config.ts'
 import { db } from './db.ts'
 
-db.query('CREATE TABLE IF NOT EXISTS`log` ( `id` INT NOT NULL AUTO_INCREMENT , `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `roomId` INT NOT NULL , `uid` BIGINT UNSIGNED NOT NULL , `nickname` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, `text` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+db.query('CREATE TABLE IF NOT EXISTS`log` ( `id` INT NOT NULL AUTO_INCREMENT , `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `roomId` INT NOT NULL , `uid` BIGINT UNSIGNED NOT NULL , `nickname` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, `text` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB')
 let ws = new WebSocket(`ws://127.0.0.1:${config.apiPort}`)
 const APIMsgHandler = new EventEmitter()
 
 interface IMessage {
     cmd: string
+    room: number
     data: any
 }
 
@@ -21,7 +22,7 @@ function registerCallback() {
     })
     ws.addEventListener('message', (event) => {
         const msg: IMessage = JSON.parse(event.data)
-        APIMsgHandler.emit(msg.cmd, msg.data)
+        APIMsgHandler.emit(msg.cmd, msg.room, msg.data)
     })
 }
 
@@ -29,29 +30,26 @@ registerCallback()
 
 APIMsgHandler.on('AUTH', (result: string) => {
     if (result === 'AUTHED') {
-        ws.send(JSON.stringify({ cmd: "ROOMID", data: config.token }));
+        ws.send(JSON.stringify({ cmd: "ROOMID", data: config.token }))
     } else {
-        console.log('[弹幕日志插件] 认证失败');
+        console.log('[弹幕日志插件] 认证失败')
     }
 })
 
-APIMsgHandler.on('ROOMID', (roomId: number) => {
-    console.log(`[弹幕日志插件] 工作在${roomId}`);
-    APIMsgHandler.on('DANMU_MSG', (data: Array<any>) => {
-        db.execute('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [roomId, data[2][0], data[2][1], data[1]])
-            .catch((reason: Error) => {
-                console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`);
-                console.log((reason as Error).message);
-            });
+APIMsgHandler.on('DANMU_MSG', (room: number, data: Array<any>) => {
+    db.execute('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [room, data[2][0], data[2][1], data[1]])
+        .catch((reason: Error) => {
+            console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`)
+            console.log((reason as Error).message)
+        })
+})
 
-    });
-    APIMsgHandler.on('SUPER_CHAT_MESSAGE', (data: any) => {
-        db.execute('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [roomId, data['uid'], data['user_info']['uname'], data['message']])
-            .catch((reason: Error) => {
-                console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`);
-                console.log((reason as Error).message);
-            });
-    });
+APIMsgHandler.on('SUPER_CHAT_MESSAGE', (room: number, data: any) => {
+    db.execute('INSERT INTO `log`(`roomId` ,`uid`, `nickname`, `text`) VALUES(?, ?, ?, ?)', [room, data['uid'], data['user_info']['uname'], data['message']])
+        .catch((reason: Error) => {
+            console.error(`[弹幕日志插件] 写日志失败 弹幕信息 ${data[2][0]} ${data[2][1]} ${data[1]}`)
+            console.log((reason as Error).message)
+        })
 })
 
 Deno.addSignalListener('SIGTERM', () => {
@@ -63,5 +61,5 @@ Deno.addSignalListener('SIGINT', () => {
 })
 
 globalThis.addEventListener('unload', () => {
-    console.log(`[弹幕日志插件] 插件退出`);
+    console.log(`[弹幕日志插件] 插件退出`)
 })
